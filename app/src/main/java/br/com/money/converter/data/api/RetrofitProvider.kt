c63760f24b5367c7617f514586760238d2c00503
Thiago dos Santos Gomes <tgomes1@pagbank.com>
2026-01-04 16:13:56 -0300

    [feat] corrige a resposta nulla do endpoint

--- app/src/main/java/br/com/money/converter/data/api/RetrofitProvider.kt
+++ app/src/main/java/br/com/money/converter/data/api/RetrofitProvider.kt
@@ -1,6 +1,7 @@
 package br.com.money.converter.data.api
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -9,7 +10,12 @@ object RetrofitProvider {
     private const val BASE_URL = "http://10.0.2.2:8080"
 
     private val okHttpClient: OkHttpClient by lazy {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
         OkHttpClient.Builder()
+            .addInterceptor(logging)
             .build()
     }
 
